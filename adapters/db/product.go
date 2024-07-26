ccfabb2c27dc6cf45fcde08c5d229143aa6d0b85
BrunoSSantana <brunoosouza15@gmail.com>
2024-07-25 21:16:05 -0300

    test: product_db

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -11,7 +11,11 @@ type ProductDB struct {
 	db *sql.DB
 }
 
-func (p ProductDB) Get(id string) (application.ProductInterface, error) {
+func NewProductDb(db *sql.DB) *ProductDB {
+	return &ProductDB{db: db}
+}
+
+func (p *ProductDB) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
 
 	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
@@ -25,4 +29,74 @@ func (p ProductDB) Get(id string) (application.ProductInterface, error) {
 	}
 
 	return &product, nil
+}
+
+func (p *ProductDB) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+
+	p.db.QueryRow( /* sql */ `select id from products where id=?`, product.GetID()).Scan(&rows)
+	if rows == 0 {
+		_, err := p.create(product)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	_, err := p.update(product)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDB) create(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare( /* sql */ `insert into products(id, name, price, status) values (?,?,?,?)`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(
+		product.GetID(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDB) update(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare( /* sql */ `update products set name = ?, price = ?, status = ? where id = ?`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+		product.GetID(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
 }
