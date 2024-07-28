8f6d34e45a8f2ef6a3b28fc2e967718a43032dd5
BrunoSSantana <brunoosouza15@gmail.com>
2024-07-27 22:21:24 -0300

    feat: :sparkles: implement cli adapter with cobra-cli

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
+/*
+Copyright © 2024 BrunoSSantana <brunoosouza15@gmail.com>
+*/
 package main
 
-import (
-	"database/sql"
-
-	dbAdapter "github.com/BrunoSSanta/hexagonal-go/adapters/db"
-	"github.com/BrunoSSanta/hexagonal-go/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/BrunoSSanta/hexagonal-go/cmd"
 
 func main() {
-
-	db, _ := sql.Open("sqlite3", "sqlite.db")
-	productDbAdapter := dbAdapter.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-
-	product, _ := productService.Create("Product Example", 230)
-
-	productService.Enable(product)
-
+	cmd.Execute()
 }
